a1364bc1d1794ec01c91092c658ba302bac35625
Hugo Gomes <hugo@sketch.com>
2020-04-27 13:22:21 +0100

    Add assistant naming-conventions

--- packages/naming-conventions/src/index.ts
+++ packages/naming-conventions/src/index.ts
@@ -1,23 +1,33 @@
-import type { Assistant } from '@sketch-hq/sketch-assistant-types'
+import { Assistant } from '@sketch-hq/sketch-assistant-types'
+import core from '@sketch-hq/sketch-assistant-core-rules'
 
-const assistant: Assistant = async () => {
+const assistant: Assistant = async (env) => {
+  const coreAssistant = await core(env)
   return {
     name: '@sketch-hq/sketch-naming-conventions-assistant',
-    rules: [
-      {
-        rule: async (context) => {
-          context.utils.report({
-            message: 'Hello world',
-          })
-        },
-        name: '@sketch-hq/sketch-naming-conventions-assistant/hello-world',
-        title: 'Hello World',
-        description: 'Emits a violation with a hello world message',
-      },
-    ],
+    rules: coreAssistant.rules,
     config: {
       rules: {
-        '@sketch-hq/sketch-naming-conventions-assistant/hello-world': { active: true },
+        '@sketch-hq/sketch-assistant-core-rules/name-pattern-pages': {
+          active: true,
+          forbidden: [],
+          allowed: ['^(?![a-zA-Z0-9,.-_;!"#$%&/()@Û£><|\\¼»`«]).'],
+        },
+        '@sketch-hq/sketch-assistant-core-rules/name-pattern-artboards': {
+          active: true,
+          forbidden: [],
+          allowed: ['^(?![a-zA-Z;!"#$%&/()@Û£><|\\¼»`«]).'],
+        },
+        '@sketch-hq/sketch-assistant-core-rules/name-pattern-groups': {
+          active: true,
+          allowed: [],
+          forbidden: ['^Group$', 'Group Copy'],
+        },
+        '@sketch-hq/sketch-assistant-core-rules/name-pattern-symbols': {
+          active: true,
+          allowed: ['(^.+)(([/].*)+)\\w$'],
+          forbidden: [],
+        },
       },
     },
   }
