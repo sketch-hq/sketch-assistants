d8c0307c61dd05b240d961c4122c60a43fbae212
jedrichards <jedrichards@gmail.com>
2020-05-22 13:17:09 +0100

    fix: exported-layers-normal-blend-mode

--- assistants/core/src/rules/exported-layers-normal-blend-mode/__tests__/index.ts
+++ assistants/core/src/rules/exported-layers-normal-blend-mode/__tests__/index.ts
@@ -2,7 +2,6 @@ import { testRule } from '../../../test-helpers'
 
 describe('exported-layers-normal-blend-mode', () => {
   test('no violations for exported layers without blend modes', async (): Promise<void> => {
-    expect.assertions(2)
     const { violations, errors } = await testRule(
       __dirname,
       './exported-layer.sketch',
@@ -13,7 +12,6 @@ describe('exported-layers-normal-blend-mode', () => {
   })
 
   test('finds violations for exported layers with blended styles', async (): Promise<void> => {
-    expect.assertions(2)
     const { violations, errors } = await testRule(
       __dirname,
       './blended-styles.sketch',
@@ -24,13 +22,22 @@ describe('exported-layers-normal-blend-mode', () => {
   })
 
   test('finds violations for exported blended layers', async (): Promise<void> => {
-    expect.assertions(2)
     const { violations, errors } = await testRule(
       __dirname,
       './blended-layer.sketch',
       'exported-layers-normal-blend-mode',
     )
     expect(violations).toHaveLength(1)
+    expect(errors).toHaveLength(0)
+  })
+
+  test('no violations for blended layer exported only as bitmaps', async (): Promise<void> => {
+    const { violations, errors } = await testRule(
+      __dirname,
+      './blended-layer-bitmap-export.sketch',
+      'exported-layers-normal-blend-mode',
+    )
+    expect(violations).toHaveLength(0)
     expect(errors).toHaveLength(0)
   })
 })
