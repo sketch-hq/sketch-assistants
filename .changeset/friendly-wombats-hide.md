4fce9afb34a37198cc612fc7f86f871226dce7a4
jedrichards <jedrichards@gmail.com>
2020-07-08 14:23:27 +0100

    fix: cli type error

---
+++ .changeset/friendly-wombats-hide.md
@@ -0,0 +1,5 @@
+---
+'@sketch-hq/sketch-assistant-cli': patch
+---
+
+Fix build error.
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -312,12 +312,13 @@ const formatResults = (cliResults: CliResults): string => {
         append(6, `Detail: ${chalk.grey(violation.message)}`)
         append(6, `Severity: ${chalk.grey(ViolationSeverity[violation.severity])}`)
         append(6, `Rule: ${chalk.grey(violation.ruleName)}`)
-        for (const location of violation.locations) {
-          const locations = []
-          if (location.objectName) locations.push(`Layer : ${location.objectName}`)
-          if (location.pointer) locations.push(`Pointer : ${location.pointer}`)
-          if (location.objectId) locations.push(`Id : ${location.objectId}`)
-          append(6, `Location: ${chalk.grey(locations.join(' | '))}`)
+        for (const object of violation.objects) {
+          const objects = []
+          if (object.name) objects.push(`Name : ${object.name}`)
+          if (object.pointer) objects.push(`Pointer : ${object.pointer}`)
+          if (object.id) objects.push(`Id : ${object.id}`)
+          if (object.class) objects.push(`Class : ${object.id}`)
+          append(6, `Object: ${chalk.grey(objects.join(' | '))}`)
         }
         append()
       }
